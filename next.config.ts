2ce5fdc4115bb2b8c305427acd5d799534cbecda
Roman Bellisari <romanbellisari@gmail.com>
2026-04-27 10:51:36 -0400

    update dist dir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: ".next-output",
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
